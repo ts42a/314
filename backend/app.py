65c904a9f7cce5a7bdffaf741a2236781b03f730
Azokaz2 <azoka614@gmail.com>
2025-06-06 11:13:50 +1000

    Implement CD Test for Deployment

--- backend/app.py
+++ backend/app.py
@@ -32,6 +32,10 @@ db.init_app(app) # bind db obj from models.py
 login_manager = LoginManager(app)
 login_manager.login_view = 'home'
 
+@app.route("/")
+def index():
+    return "App is running"
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
